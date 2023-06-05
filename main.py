b90f53a5b3b11c85d77c085644be2614afb5431b
Yudha Nurul Alfian <67511620+yudha-nurul@users.noreply.github.com>
2023-06-05 14:28:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,43 @@
 # from comtypes.client import GetActiveObject
+"""
 import sys
 import os
 from contextlib import redirect_stdout
-import win32com.client
 
+# import pymsgbox
+import win32com.client
 ps = win32com.client.Dispatch("PowerShape.Application")
 ps.Visible = True
-
-def obj():
-    count = int(ps.evaluate('selection.number'))
+"""
+def copy_to_point():
+    # count = int(ps.evaluate('selection.number'))
+    count = 10
     if count == 0:
         print('select at leas 1 dimension')
         sys.exit()
-    # print(f'jumlah dimensi yang diselect adalah {count}')
+    print(f'jumlah object yang diselect adalah {count}')
 
-    nama = (ps.evaluate('SELECTION.NAMES'))
+    # nama = (ps.evaluate('SELECTION.NAMES'))
+    nama = '{ Solid; Point }'
     nama = nama.replace(" ", "")
     nama = nama.replace("{", "")
     nama = nama.replace("}", "")
     nama = nama.split(';')
-    # print(nama) # this is list of dimension names
+    print(nama) # this is list of object names
+    '''
     for i in nama:
         print('select clearlist')
-        print(f'add dimension "{i}"')
+        print(f'add ---disinitipeobjecnya--- "{i}"')
         dim_value = ps.evaluate(f'dimension[{i}].value')
         # print(dim_value)
         # print(type(dim_value))
-
-        if 0 < dim_value <= 3:
-            print('DIALOG TOLEDIT')
-            print('TOLVALUE1 0.01')
-            print('TOLVALUE2 - 0.01')
-            print('TOOLBAR DIMATTRIBUTES DISMISS')
-        elif 3 < dim_value <= 6:
-            print('DIALOG TOLEDIT')
-            print('TOLVALUE1 0.012')
-            print('TOLVALUE2 - 0.012')
-            print('TOOLBAR DIMATTRIBUTES DISMISS')
-
-# blm selesai yg ini
-
+    '''
+'''
 def go_plusmin():
     cwd = os.getcwd()
     with open('go.mac', 'w') as file:
         with redirect_stdout(file):
-            run = f'{obj()}'
+            run = f'{dim_tolerance_plusmin()}'
     assert isinstance(ps, object)
     ps.exec(f'MACRO RUN "{cwd}\go.mac"')
-
-"""
------------------------------------------------
-#  :
-
------------------------------------------------
-"""
+'''
