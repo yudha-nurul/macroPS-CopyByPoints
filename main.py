9b1aa7c194fbe0987fd711c4c202cbca3afaa9d4
Yudha Nurul Alfian <67511620+yudha-nurul@users.noreply.github.com>
2023-06-07 15:16:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-    # from comtypes.client import GetActiveObject
+# from comtypes.client import GetActiveObject
 """
 import sys
 import os
@@ -10,23 +9,22 @@ import win32com.client
 ps = win32com.client.Dispatch("PowerShape.Application")
 ps.Visible = True
 """
+
+
 def copy_to_points():
     # count = int(ps.evaluate('selection.number'))
     count = 10
     if count == 0:
         print('select at leas 1 object')
         sys.exit()
-
-	
-    #tipe = (ps.evaluate('SELECTION.TYPES'))
+    # tipe = (ps.evaluate('SELECTION.TYPES'))
     tipe = '{ Solid; Point; 3dpoint; Point}'
     tipe = tipe.replace(" ", "")
     tipe = tipe.replace("{", "")
     tipe = tipe.replace("}", "")
     tipe = tipe.split(';')
-    print(tipe) # this is list of object names
-    
-    
+    print(tipe)  # this is list of object names
+
     # nama = (ps.evaluate('SELECTION.NAMES'))
     nama = '{ 1; 26; 30; 99}'
     nama = nama.replace(" ", "")
@@ -35,29 +33,17 @@ def copy_to_points():
     nama = nama.split(';')
     print(nama)  # this is list of object names
     start = 0
-    print(f'EDIT MOVE')
-    print(f'KEEP')
-    print(f'MOVEORIGIN')
-    print(f'0, 0, 0,')  # start point
+    print('EDIT MOVE')
+    print('KEEP')
+    print('MOVEORIGIN')
+    print('0, 0, 0,')  # start point
     for i in tipe:
         if i == 'Point':
             print(f'add {i} "{nama[start]}"')
+            print('APPLY')
         start += 1
-    
-    
-    
-    
-    
-    
-    
-    '''
-    for i in nama:
-        print('select clearlist')
-        print(f'add ---disinitipeobjecnya--- "{i}"')
-        dim_value = ps.evaluate(f'dimension[{i}].value')
-        # print(dim_value)
-        # print(type(dim_value))
-    '''
+    print('DISMISS')
+
 '''
 def go_plusmin():
     cwd = os.getcwd()
@@ -68,4 +54,24 @@ def go_plusmin():
     ps.exec(f'MACRO RUN "{cwd}\go.mac"')
 '''
 if __name__ == '__main__':
-	copy_to_points()
+    copy_to_points()
+
+"""
+---------------------------------------------------
+ini adalah move copy ori nya :
+add Solid "1"
+EDIT MOVE
+KEEP
+MOVEORIGIN
+0, 0, 0,
+-50, -0, 0,
+APPLY
+-46, 1, 0,
+APPLY
+4, 42, 0,
+APPLY
+43, -1, 0,
+APPLY
+DISMISS
+----------------------------------------------------------------------------------
+"""
